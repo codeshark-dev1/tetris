1a62dabfda79a7695d232e310b477f0f7decfbbe
Montieh <wfmaijer@gmail.com>
2024-10-20 15:02:35 +0200

    Add files via upload

--- GameState.cs
+++ GameState.cs
@@ -2,12 +2,16 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using System;
+using Microsoft.Xna.Framework.Media;
 
 namespace Tetris
 {
     class GameState
     {
+        private Song song;
         public SpriteFont font;
+
+        float fadeIncrement = 0.00075f;
 
         public enum state
         {
@@ -16,12 +20,39 @@ namespace Tetris
 
         public state currentState = state.Playing;
 
+        public GameState()
+        {
+            song = TetrisGame.ContentManager.Load<Song>("Running Errands - TrackTribe");
+            MediaPlayer.Volume = 0.1f;
+
+        }
+
         public void CheckState(SpriteBatch spriteBatch)
         {
             if (currentState == state.GameOver)
             {
                 spriteBatch.DrawString(font, "Game Over", new Vector2(600 - font.MeasureString("Game Over").X / 2, 800 / 2), Color.Black);
                 spriteBatch.DrawString(font, "\nPress spacebar to play again!", new Vector2(600 - font.MeasureString("\nPress spacebar to play again!").X / 2, 800 / 2), Color.Black);
+
+                if (MediaPlayer.Volume >= 0)
+                {
+                    MediaPlayer.Volume -= fadeIncrement;
+                }
+                else
+                {
+                    MediaPlayer.Stop();
+                }
+            }
+            else
+            {
+                if (MediaPlayer.Volume <= 0.1)
+                {
+                    MediaPlayer.Volume += fadeIncrement;
+                }
+                    if (MediaPlayer.State != MediaState.Playing)
+                {
+                    MediaPlayer.Play(song);
+                }
             }
         }
 
